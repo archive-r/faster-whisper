d9d5301bfac4a28f15618c3e05e75898b149b65f
archive-r <comienzo2093@gmail.com>
2023-07-23 21:45:52 +0900

    Remove should_skip

--- faster_whisper/transcribe.py
+++ faster_whisper/transcribe.py
@@ -404,27 +404,27 @@ class WhisperModel:
                 compression_ratio,
             ) = self.generate_with_fallback(encoder_output, prompt, tokenizer, options)
 
-            if not options.no_speech_threshold is not None:
-                # no voice activity check
-                should_skip = result.no_speech_prob > options.no_speech_threshold
+            # if not options.no_speech_threshold is not None:
+            #     # no voice activity check
+            #     should_skip = result.no_speech_prob > options.no_speech_threshold
 
-                if (
-                    options.log_prob_threshold is not None
-                    and avg_logprob >= options.log_prob_threshold
-                ):
-                    # don't skip if the logprob is high enough, despite the no_speech_prob
-                    should_skip = False
+            #     if (
+            #         options.log_prob_threshold is not None
+            #         and avg_logprob >= options.log_prob_threshold
+            #     ):
+            #         # don't skip if the logprob is high enough, despite the no_speech_prob
+            #         should_skip = False
 
-                if should_skip:
-                    self.logger.debug(
-                        "No speech threshold is met (%f > %f)",
-                        result.no_speech_prob,
-                        options.no_speech_threshold,
-                    )
+            #     if should_skip:
+            #         self.logger.debug(
+            #             "No speech threshold is met (%f > %f)",
+            #             result.no_speech_prob,
+            #             options.no_speech_threshold,
+            #         )
 
-                    # fast-forward to the next segment boundary
-                    seek += segment_size
-                    continue
+            #         # fast-forward to the next segment boundary
+            #         seek += segment_size
+            #         continue
 
             tokens = result.sequences_ids[0]
 
