1d9f15dc9a2d32e9dfb492bc9d66851bfde2a3b9
archive-r <comienzo2093@gmail.com>
2023-07-23 19:42:22 +0900

    Revert

--- faster_whisper/transcribe.py
+++ faster_whisper/transcribe.py
@@ -728,8 +728,6 @@ class WhisperModel:
             if (
                 options.no_speech_threshold is not None
                 and result.no_speech_prob > options.no_speech_threshold
-                and avg_logprob >= options.log_prob_threshold
-                and compression_ratio <= options.compression_ratio_threshold
             ):
                 needs_fallback = False  # silence
 
